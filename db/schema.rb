ad70bb812b3bc9f56802efd8706e78bec3385ebf
nakatatsu77 <nakatatsu1177@gmail.com>
2024-11-19 23:55:08 +0900

    投稿一覧機能の実装 #15

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_15_110719) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_16_143709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +31,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_15_110719) do
     t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
