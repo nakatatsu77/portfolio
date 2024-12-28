b6b4dec8afc0195e4f9962d909cdeb7241ca8a03
nakatatsu77 <nakatatsu1177@gmail.com>
2024-12-28 23:46:51 +0900

    各ページにフラッシュメッセージを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :success then "bg-primary"
+    when :danger  then "bg-secondary"
+    else "bg-info"
+    end
+  end
 end
