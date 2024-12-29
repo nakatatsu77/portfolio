3be14cba0eef19d6fb24033963411ef8b0ce137e
nakatatsu77 <nakatatsu1177@gmail.com>
2024-12-29 23:54:31 +0900

    フォーム入力時エラー情報を個別表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def flash_background_color(type)
     case type.to_sym
     when :success then "bg-primary"
-    when :danger  then "bg-secondary"
+    when :danger  then "bg-error"
     else "bg-info"
     end
   end
