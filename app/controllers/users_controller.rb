26ca664352440e0dda09b7344e49fdd5436090c4
nakatatsu77 <nakatatsu1177@gmail.com>
2024-12-31 21:08:17 +0900

    オートログイン機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, success: t("users.create.success")
     else
       flash.now[:danger] = t("users.create.failure")
